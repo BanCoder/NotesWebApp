22da1419e025cecfa10d1560923ad68b549a6e40
Vladimir Belskiy <belskiy548@gmail.com>
2025-12-11 21:30:42 +0300

    Add controllers to post query

---
+++ NotesWebApp/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesWebApp
+{
+	[ApiController]
+	[Route("Note")]
+	public class NoteController(INoteService noteService): ControllerBase
+	{
+		[HttpPost]
+		public async Task<IActionResult> CreateAsync(string text)
+		{
+			await noteService.CreateAsync(text);
+			return NoContent(); 
+		}
+	}
+}
--- NotesWebApp/Program.cs
+++ NotesWebApp/Program.cs
@@ -1,5 +1,13 @@
-using DataAccess; 
+using DataAccess;
+using BusinessLogic; 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDataAccess(); 
+builder.Services.AddDataAccess();
+builder.Services.AddBusinessLogic();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(); 
+
 var app = builder.Build();
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(); 
 app.Run();
